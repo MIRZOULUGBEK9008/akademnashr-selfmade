66490f8cdc6592f491cf513fa8fb2929c0dc1878
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-08-19 15:39:13 +0500

    Add responsibility into section features and sale

--- js/main.js
+++ js/main.js
@@ -4,5 +4,7 @@ elNav = document.querySelector('.js-nav');
 if (elButton) {
   elButton.addEventListener('click', function (){
     elNav.classList.toggle('sitenav__list-wrapper--open');
+    document.body.classList.toggle('body--overflow')
   });
+
 }
