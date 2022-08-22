aa0541b304030a9c725d7659b860f68c9f8bffc5
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-08-22 19:22:28 +0500

    Restyle site-header in responsibility

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 const elButton = document.querySelector('.js-menu'),
-elNav = document.querySelector('.js-nav');
+elNav = document.querySelector('.js-nav'),
+elOpen = document.querySelector('.js-open'),
+elClose = document.querySelector('.js-close');
 
 if (elButton) {
   elButton.addEventListener('click', function (){
     elNav.classList.toggle('sitenav__list-wrapper--open');
-    document.body.classList.toggle('body--overflow')
+    document.body.classList.toggle('body--overflow');
+    elOpen.classList
+    .toggle('menu__img-open--block')
+    elClose.classList
+    .toggle('menu__img-close--block')
   });
 
 }
