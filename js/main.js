7945fdf7d43d786b0960afad82bb4baa5c146e70
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-08-23 10:25:37 +0500

    Add loader

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-const elButton = document.querySelector('.js-menu'),
+const elLoader = document.querySelector('.lds-dual-ring-wrapper'),
+elButton = document.querySelector('.js-menu'),
 elNav = document.querySelector('.js-nav'),
 elOpen = document.querySelector('.js-open'),
 elClose = document.querySelector('.js-close');
 
+
+// Loader
+document.addEventListener('DOMContentLoaded', function (){
+  setTimeout(function(){
+    elLoader.classList.add('lds-dual-ring-wrapper--none');
+  }, 800);
+});
+
+// Navbar
 if (elButton) {
   elButton.addEventListener('click', function (){
     elNav.classList.toggle('sitenav__list-wrapper--open');
